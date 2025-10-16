66076a40449f06cdf9c4f41a8100896fc9aa3e10
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-10-16 16:18:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const emailRegex = /^[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 function validEmail(str) {
-  //your JS code here.
+  //your JS code here.	
+	return emailRegex.test(str);
 }
 
-// Do not change the code below.
+// Do not change the code below
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
